b562fa64c50ba905e401e77f5e0d7c1ac5cbee0c
Olayinka Oginni <olayinkaoginni2016@gmail.com>
2022-05-02 23:46:57 +0200

    useEffect

--- src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
+++ src/tutorial/2-useEffect/setup/3-useEffect-fetch-data.js
@@ -1,9 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
-const url = 'https://api.github.com/users';
+const url = 'https://api.github.com/users'
 
 const UseEffectFetchData = () => {
-  return <h2>fetch data</h2>;
-};
+  const [users, setUsers] = useState([])
+  const getUser = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    console.log(users)
+    setUsers(users)
+  }
+  useEffect(() => {
+    getUser()
+  }, [])
+  return (
+    <>
+      <h2>Get User</h2>
+      <ul className='users'>
+        {users.map((user) => {
+          const { id, login, avatar_url, html_url } = user
+          return (
+            <li key={id}>
+              <img src={avatar_url} alt='login' />
+              <div>
+                <h4>{login}</h4>
+                <a href={html_url}>Profile</a>
+              </div>
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
 
-export default UseEffectFetchData;
+export default UseEffectFetchData
--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,36 @@
-import React, { useState, useEffect } from 'react';
-const url = 'https://api.github.com/users/QuincyLarson';
+import React, { useState, useEffect } from 'react'
+const url = 'https://api.github.com/users/QuincyLarson'
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
-};
+  const [isLoading, setLoading] = useState(true)
+  const [isError, setError] = useState(false)
+  const [users, setUsers] = useState([])
+  const getUser = async () => {
+    const response = await fetch(url)
+    const users = await response.json()
+    if (response.status >= 200 && response.status <= 299) {
+      setLoading(false)
+      setUsers(users)
+    } else {
+      setLoading(false)
+      setError(true)
+      throw new Error(response.statusText)
+    }
+  }
+  useEffect(() => {
+    getUser()
+  }, [])
+  if (isLoading) {
+    return <h1>Loading..</h1>
+  }
+  if (isError) {
+    return <h1>Error..</h1>
+  }
 
-export default MultipleReturns;
+  return (
+    <>
+      <h3>{users.login}</h3>
+    </>
+  )
+}
+
+export default MultipleReturns
